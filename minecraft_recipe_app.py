df95eb70a48c37f5674b8709c815c7e40f5a9148
Ikari <zachary@zachary-miller.com>
2023-12-17 03:20:59 -0500

    NESTED RECIPES!

--- minecraft_recipe_app.py
+++ minecraft_recipe_app.py
@@ -1,87 +1,96 @@
 import sqlite3
 import json
 import modules.c_crafting_block
-import modules.c_recipe
+import modules.c_recipe as rcp
 import modules.database_ops as db
+import math
 
-class Recipe:
-    def __init__(self, name, crafting_block="ctable3", shaped=False, slots=None, ingredients=None):
-        self.name = name
-        self.crafting_block = crafting_block
-        self.shaped = shaped
-        self.slots = slots if slots else {}
-        self.ingredients = ingredients if ingredients else {}
-
-    def to_json(self):
-        return json.dumps(self.__dict__)
-
-    @staticmethod
-    def from_json(json_str):
-        data = json.loads(json_str)
-        return Recipe(name=data['name'], crafting_block=data['crafting_block'], 
-                      shaped=data['shaped'], slots=data['slots'], ingredients=data['ingredients'])
 def create_recipe():
-    name = input("Enter the name of the item (e.g., Chest): ").strip()
-    if not name:
-        print("Invalid input: Name cannot be empty.")
-        return
+    name = input("Enter the name of the item (e.g., Aqueous Accumulator): ")
+    crafting_block = "ctable3"  # Default or choose from available blocks
+    rec_output_count = int(input("Enter the number of items produced by this recipe (default 1): ") or "1")
+    shaped = input("Is the recipe shaped? (yes/no): ").lower() == 'yes'
 
-    shaped_input = input("Is the recipe shaped? (yes/no): ").lower().strip()
-    shaped = shaped_input == 'yes'
-    
-    crafting_block = "ctable3"  # This could be expanded to choose from available crafting blocks
-
-    slots = {}
+    slots = {}  # Add logic for slot-based recipes if needed
     ingredients = {}
+    nested_recipes = {}
+
+    existing_recipes = db.list_recipes()  # Retrieve existing recipes from the database
 
     while True:
-        ingredient = input("Enter an ingredient (or type 'done' to finish): ").strip()
-        if ingredient.lower() == 'done':
+        choice = input("Add ingredient (1) or use existing recipe (2) or 'done': ")
+        if choice == 'done':
             break
-        if not ingredient:
-            print("Invalid input: Ingredient cannot be empty.")
-            continue
-
-        try:
+        elif choice == '1':
+            ingredient = input("Enter an ingredient: ")
             quantity = int(input(f"Enter the quantity of {ingredient} needed: "))
-            if quantity <= 0:
-                raise ValueError
-        except ValueError:
-            print("Invalid input: Quantity must be a positive integer.")
-            continue
+            ingredients[ingredient] = quantity
+        elif choice == '2':
+            if existing_recipes:
+                for recipe_id, recipe_name, output_count in existing_recipes:
+                    print(f"{recipe_id}. {recipe_name} (makes {output_count})")
+                selected_recipe_id = int(input("Select the ID of the recipe to use as an ingredient: "))
+                # Find the selected recipe details
+                selected_recipe = next((name for id, name, count in existing_recipes if id == selected_recipe_id), None)
+                if selected_recipe:
+                    final_quantity_needed = int(input(f"Enter the quantity of {selected_recipe} needed: "))
+                    nested_recipes[selected_recipe_id] = final_quantity_needed  # Store the final product quantity
+                else:
+                    print("Invalid recipe ID.")
+            else:
+                print("No existing recipes available.")
 
-        ingredients[ingredient] = quantity
-
-    recipe = Recipe(name, crafting_block, shaped, slots, ingredients)
+    recipe = rcp.Recipe(
+    name=name,
+    crafting_block=crafting_block,
+    output_count=int(rec_output_count),
+    shaped=shaped,
+    slots=slots,
+    ingredients=ingredients,
+    nested_recipes=nested_recipes
+)
     db.save_recipe_to_db(recipe)
     return recipe
 
 
 def calculate_ingredients(recipe_name, desired_quantity):
+    def calculate(recipe, multiplier):
+        ingredients_needed = {}
+        steps = []
+
+        # Calculate ingredients for the base recipe
+        for ingredient, quantity in recipe.ingredients.items():
+            total_quantity = quantity * multiplier
+            ingredients_needed[ingredient] = ingredients_needed.get(ingredient, 0) + total_quantity
+
+        # Calculate ingredients for nested recipes
+        for nested_id, final_quantity_needed in recipe.nested_recipes.items():
+            nested_recipe = db.fetch_recipe_by_id(nested_id)
+            if nested_recipe:
+                nested_multiplier = math.ceil(final_quantity_needed / nested_recipe.output_count)
+                nested_ingredients, nested_steps = calculate(nested_recipe, nested_multiplier)
+                steps.append((nested_recipe.name, nested_multiplier, nested_recipe.output_count, nested_steps))
+                for ing, qty in nested_ingredients.items():
+                    ingredients_needed[ing] = ingredients_needed.get(ing, 0) + qty
+
+        return ingredients_needed, steps
+
+    def print_steps(steps):
+        for step_name, step_multiplier, step_output, nested_steps in steps:
+            print(f"- {step_multiplier} Recipe {step_name} (Output: {step_output}, " +
+                  ", ".join([f"{qty} {ing}" for ing, qty in db.fetch_recipe(step_name).ingredients.items()]) + ")")
+            print_steps(nested_steps)  # Recursively print nested steps
+
     recipe = db.fetch_recipe(recipe_name)
     if recipe:
-        print(f"\nTo make {desired_quantity} {recipe.name}(s), you need:")
-        for ingredient, quantity in recipe.ingredients.items():
-            total_quantity = quantity * desired_quantity
-            print(f"- {total_quantity} {ingredient}")
+        print(f"\nTo make {desired_quantity} {recipe.name}(s), you need to first make:")
+        total_ingredients, steps = calculate(recipe, desired_quantity)
+        print_steps(steps)
+        print("\nTotal:")
+        for ingredient, quantity in total_ingredients.items():
+            print(f"- {quantity} {ingredient}")
     else:
         print("Recipe not found.")
-
-def list_recipes():
-    conn = sqlite3.connect('minecraft_recipes.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT id, name FROM recipes')
-    rows = cursor.fetchall()
-    conn.close()
-
-    if rows:
-        print("\nAvailable Recipes:")
-        for idx, (recipe_id, name) in enumerate(rows, start=1):
-            print(f"{idx}. {name}")
-        return rows
-    else:
-        print("No recipes available.")
-        return []
 
 def main():
     while True:
@@ -95,13 +104,18 @@ def main():
         if choice == "1":
             create_recipe()
         elif choice == "2":
-            list_recipes()
+            existing_recipes = db.list_recipes()
+            print("\nAvailable Recipes:")
+            for recipe_number, recipe_name, output_count in existing_recipes:
+                print(f"{recipe_number}. {recipe_name} (Output: {output_count})")
         elif choice == "3":
-            recipes = list_recipes()
+            recipes = db.list_recipes()
             if recipes:
+                for recipe_number, recipe_name, output_count in recipes:
+                    print(f"{recipe_number}. {recipe_name} (Output: {output_count})")
                 recipe_choice = int(input("Enter the number of the recipe to calculate: "))
                 if 1 <= recipe_choice <= len(recipes):
-                    recipe_id, recipe_name = recipes[recipe_choice - 1]
+                    recipe_id, recipe_name, _ = recipes[recipe_choice - 1]
                     desired_quantity = int(input(f"Enter the number of {recipe_name}s you want to make: "))
                     calculate_ingredients(recipe_name, desired_quantity)
                 else:
--- modules/database_ops.py
+++ modules/database_ops.py
@@ -15,7 +15,8 @@ def setup_database(conn=None):
             name TEXT NOT NULL,
             ingredients TEXT NOT NULL,
             shaped BOOLEAN NOT NULL,
-            crafting_block TEXT NOT NULL
+            crafting_block TEXT NOT NULL,
+            output_count INTEGER NOT NULL DEFAULT 1
         )
     ''')
     conn.commit()
@@ -30,8 +31,8 @@ def save_recipe_to_db(recipe, conn=None):
         should_close = True
 
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO recipes (name, ingredients, shaped, crafting_block) VALUES (?, ?, ?, ?)',
-                   (recipe.name, recipe.to_json(), recipe.shaped, recipe.crafting_block.name))
+    cursor.execute('INSERT INTO recipes (name, ingredients, shaped, crafting_block, output_count) VALUES (?, ?, ?, ?, ?)',
+                   (recipe.name, recipe.to_json(), recipe.shaped, recipe.crafting_block.name, recipe.output_count))
     conn.commit()
 
     if should_close:
@@ -54,3 +55,40 @@ def fetch_recipe(recipe_name, conn=None):
         return Recipe.from_json(row[0])
     else:
         return None
+    
+
+def fetch_recipe_by_id(recipe_id, conn=None):
+    should_close = False
+    if conn is None:
+        conn = sqlite3.connect('minecraft_recipes.db')
+        should_close = True
+
+    cursor = conn.cursor()
+    cursor.execute('SELECT ingredients FROM recipes WHERE id = ?', (recipe_id,))
+    row = cursor.fetchone()
+
+    if should_close:
+        conn.close()
+
+    if row:
+        return Recipe.from_json(row[0])
+    else:
+        return None
+
+    
+def list_recipes(conn=None):
+    should_close = False
+    if conn is None:
+        conn = sqlite3.connect('minecraft_recipes.db')
+        should_close = True
+
+    cursor = conn.cursor()
+    query = 'SELECT id, name, output_count FROM recipes'
+    cursor.execute(query)
+    recipes = cursor.fetchall()
+
+    if should_close:
+        conn.close()
+
+    return [(idx + 1, recipe[1], recipe[2]) for idx, recipe in enumerate(recipes)]
+
